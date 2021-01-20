fe99b1c4b87fea1b37f921b15dd057ffea396900
DHWANI <anushamuttoly@gmaiol.com>
2021-01-20 21:32:46 +0530

    [INTERNAL] Creating the project

---
+++ src/main/java/com/edu/service/EduSwitchServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.edu.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSwitchServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSwitchServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/service/EduSwitchServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSwitchServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
